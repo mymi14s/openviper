1ba1aad88f12320eec0d2522a35003924a727094
mymi14s <mymi14s@hotmail.com>
2026-03-07 09:27:19 +0000

    OPV-0002 added bug fixes

--- performance_matrix/benchmark_frameworks/django_blog/posts/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from .views import PostDetailView, PostListView
-
-urlpatterns = [
-    path("posts", PostListView.as_view()),
-    path("posts/<int:id>", PostDetailView.as_view()),
-]
--- performance_matrix/benchmark_frameworks/scripts/post.lua
+++
@@ -1,5 +0,0 @@
--- wrk Lua script for POST /posts benchmark
-
-wrk.method = "POST"
-wrk.headers["Content-Type"] = "application/json"
-wrk.body = '{"title":"Benchmark Post","content":"This is benchmark content for load testing purposes."}'

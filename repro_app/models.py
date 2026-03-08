d3e875f4f32a63be3a8d4d2544246a497cb5b298
mymi14s <mymi14s@hotmail.com>
2026-03-08 14:41:54 +0000

    OPV-0004 updated commands, updated tests and docs tutorial

--- repro_app/models.py
+++
@@ -1,7 +0,0 @@
-from openviper.db.models import Model
-from openviper.db import fields
-
-class MyModel(Model):
-    class Meta:
-        table_name = "my_model"
-    title = fields.CharField(max_length=100)
